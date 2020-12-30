efb1b03837436988fec660a32a87d35cda8d1aa6
zulma <zumendez7@gmail.com>
2020-12-30 14:50:20 +0100

    exo terminé

---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+// # Exercice 2 JavaScript -> Array
+
+//     - ## Créer une variable qui reçois un array avec 3 éléments -> ex: coding 15, coding 16, coding 17
+let coding = ["coding 15", "coding 16", "coding 17"];
+
+//     - ## Afficher les éléments de l'array
+console.log(coding);
+
+//     - ## Ajouter votre prenom a l'array et afficher son contenu
+coding.push('Zulma');
+console.log(coding);
